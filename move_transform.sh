6903baad29d11f7029cf9baf097c7dda17c57b36
huangyuxiang03 <huang-yx21@mails.tsinghua.edu.cn>
2023-07-29 16:30:03 +0800

    Add: upload

--- move_transform.sh
+++ move_transform.sh
@@ -3,6 +3,7 @@
 # cp /data/checkpoints/results/Alpaca-finetune/finetune-llama-Alpaca-9.pt /data/checkpoints/models
 # mv /data/checkpoints/models/finetune-llama-Alpaca-9.pt /data/checkpoints/models/checkpoint.pt
 # echo "finish copying ckpt"
-echo "start transforming ..."
-python transform_to_hf_lora.py /data/checkpoints/models/
-echo "finish transforming"
+# echo "start transforming ..."
+# python transform_to_hf_lora.py /data/checkpoints/models/
+# echo "finish transforming"
+hdfs dfs -put /data/checkpoints/models/adapter_model.bin user/huangyuxiang/alpaca-lora
