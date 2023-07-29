4b254c6cb3bd2c77042d753e7c750c85e0187288
huangyuxiang03 <huang-yx21@mails.tsinghua.edu.cn>
2023-07-29 12:01:59 +0800

    Add: prompts

--- move_transform.sh
+++ move_transform.sh
@@ -1,4 +1,8 @@
 mkdir -p /data/checkpoints/models
+echo "start copying ckpt ..."
 cp /data/checkpoints/results/Alpaca-finetune/finetune-llama-Alpaca-9.pt /data/checkpoints/models
 mv /data/checkpoints/models/finetune-llama-Alpaca-9.pt /data/checkpoints/models/checkpoint.pt
+echo "finish copying ckpt"
+echo "start transforming ..."
 python transform_to_hf_lora.py /data/checkpoints/models/
+echo "finish transforming"
