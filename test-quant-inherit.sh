7c28831590bdaef3922618c8654ee964f29830c8
huangyuxiang03 <huang-yx21@mails.tsinghua.edu.cn>
2023-07-31 20:56:50 +0800

    Add: quant inherit

--- test-quant-inherit.sh
+++ test-quant-inherit.sh
@@ -58,11 +58,11 @@ echo "result will be saved in" $result_path
 # echo "device2" $device2
 CMD="python -u main.py --model modelcenter-llama-ft --model_args 
 pretrained=${model_path},dtype="float16",\
-comp_type=pr,pet=True,recover=True,\
+comp_type=pr,pet=True,recover=False,\
 quant_config_path=/mnt/data/user/tc_agi/user/huangyuxiang/compress_configs/quantization.json,quant_ckpt_path=/mnt/data/user/tc_agi/huangyuxiang/llama-compress/llama-7b-quantization-10000,\
 pr_config_path=/mnt/data/user/tc_agi/user/huangyuxiang/compress_configs/prune.json,pr_ckpt_path=/mnt/data/user/tc_agi/user/huangyuxiang/llama-compress/llama-7b-prune-4000,\
 spr_config_path=/mnt/data/user/tc_agi/user/huangyuxiang/compress_configs/sprune.json,spr_ckpt_path=/mnt/data/user/tc_agi/user/huangyuxiang/llama-compress/llama-7b-sprune-18000,\
-cpet_path=/data/checkpoints/results/Alpaca-pr-cpet/lora_9.pt,\
+cpet_path=/mnt/data/user/tc_agi/user/huangyuxiang/alpaca-lora/alpaca.pt,\
 use_accelerate=False --num_fewshot $shot --tasks $data --batch_size auto --output_path ${result_path}/result_${name}.json"
 echo "-------Task ${i} final CMD is------"
 echo "${CMD}"
